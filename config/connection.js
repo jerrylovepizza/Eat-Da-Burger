79d5fe203c3461e38386729366215fe096989572
Renjie Dai <you@example.com>
2019-08-02 15:07:13 -0600

    first

--- config/connection.js
+++ config/connection.js
@@ -8,10 +8,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
         connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        host: "localhost",
+    
+        user: "root",
+        password: "root",
         database: "burgers_db"
     });
 };
