39430f1b39c5c1623b1ec33d82da5cbb3d7eb59f
JerryDai <50157468+jerrylovepizza@users.noreply.github.com>
2019-08-02 18:22:07 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,20 @@
-    
-// Config (MySQL)
-// =============================================================
-require("dotenv").config();
-const mysql = require("mysql");
+var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-        connection = mysql.createConnection({
-        host: "localhost",
-    
-        user: "root",
-        password: "root",
-        database: "burgers_db"
-    });
-};
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "burgers_db"
+});
 
-connection.connect();
 
-// Export Config (MySQL)
-// =============================================================
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
